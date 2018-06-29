3954b018464428676777d60f5f41532792dbd772
Rafael Oliveira <37276054+RafaelABOliveira@users.noreply.github.com>
2018-06-29 12:50:25 -0300

    Add files via upload
    Arquivos feitos para tela de login

---
+++ destino.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Recebimento de informações</title>
+    </head>
+
+    <body> 
+        <pre>
+        
+            <?php print_r($POST); ?>
+        
+        </pre>
+    </body>
+</html>
